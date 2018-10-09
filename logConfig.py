f40f509b3332d79995ce66cf177b447c0a995c7c
vikramshelke <vickyshelke95@gmail.com>
2018-10-09 19:19:28 +0530

    logConfig.txt changed to logConfig.py

---
+++ logConfig.py
@@ -0,0 +1,8 @@
+
+LOG_ENABLE     =      "True"
+BUF_SIZE       =      10000
+HOST           =      "172.30.25.7"
+PORT           =      "5557"
+LOGFILE        =      "machineLog"
+FETCHURL       =      "fetch_prd_conf"
+SENDURL        =      "machine_data_pi/get"
